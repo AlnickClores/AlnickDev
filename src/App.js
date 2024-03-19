7254076ffd0da2403483eb70f4caab29bb3b9eaf
Alnick Clores <aelnickclores@gmail.com>
2024-03-20 03:49:03 +0800

    fixed file naming

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Footer from "./components/Footer";
-import useIntersectionObserver from "./libs/intersectionObs";
+import useIntersectionObserver from "./hooks/intersectionObs";
 
 function App() {
   useIntersectionObserver();
