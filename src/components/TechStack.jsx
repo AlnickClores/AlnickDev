5b72e0a964b4077b26112d0c7b774ca3b7b66f1a
Alnick Clores <aelnickclores@gmail.com>
2024-08-16 04:29:24 +0800

    Modified the TechStack component organized the code and some simple UI changes

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { techstack } from "../data/techstack";
 
 const TechStack = () => {
   return (
@@ -6,20 +7,15 @@ const TechStack = () => {
       <h1 className="text-[#ccd6f6] text-2xl md:text-2xl lg:text-3xl font-semibold">
         Technologies I have been actively engaged with lately:
       </h1>
-      <ul className="my-3 list-disc ml-3">
-        <li className="text-[#8892b0] md:text-base lg:text-lg my-2">HTML5</li>
-        <li className="text-[#8892b0] md:text-base lg:text-lg my-2">
-          Tailwind CSS
-        </li>
-        <li className="text-[#8892b0] md:text-base lg:text-lg my-2">
-          JavaScript (ES6+)
-        </li>
-        <li className="text-[#8892b0] md:text-base lg:text-lg my-2">React</li>
-        <li className="text-[#8892b0] md:text-base lg:text-lg my-2">NodeJS</li>
-        <li className="text-[#8892b0] md:text-base lg:text-lg my-2">
-          ExpressJS
-        </li>
-        <li className="text-[#8892b0] md:text-base lg:text-lg my-2">MySql</li>
+      <ul className="grid grid-cols-2 gap-x-7 gap-y-2 my-3 ml-3 list-disc">
+        {techstack.map((stack, index) => (
+          <li
+            key={index}
+            className="text-[#45adff] font-mono text-sm md:text-base lg:text-lg"
+          >
+            {stack}
+          </li>
+        ))}
       </ul>
     </div>
   );
---
+++ src/data/techstack.jsx
@@ -0,0 +1,8 @@
+export const techstack = [
+  "Javascript (ES6+)",
+  "Node.js",
+  "React",
+  "Express",
+  "TypeScript",
+  "MySQL",
+];
